59a30175f3960a3c294c6084789a34232138b29c
WilkRhu <wilk.caetano@gmail.com>
2020-04-26 20:13:06 -0300

    retri restrict route

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: "mysql",
-  host: "localhost",
-  username: "root",
-  password: "",
-  database: "school",
+  host: "bdhost0040.servidorwebfacil.com",
+  username: "wilkcaet_school",
+  password: "@wilk2020#",
+  database: "wilkcaet_school",
   define: {
     timestamps: true,
     underscored: true,
--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ const authUser = require("./middleware/auth");
 //Rotas de usuários
 routes.get("/users/:id", authUser, userController.showUserOne);
 routes.get("/users", authUser, userController.showUser);
-routes.post("/users", authUser, userController.store);
+routes.post("/users", userController.store);
 routes.patch("/users/:id", authUser, userController.updateUser);
 routes.patch("/endUser/:user_id", authUser, userController.updateEndereco);
 routes.post("/logOn", userController.logOn);
