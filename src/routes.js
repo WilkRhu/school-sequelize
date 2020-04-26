a58a4006613be0f5c063a502578704513460b36f
WilkRhu <wilk.caetano@gmail.com>
2020-04-26 17:54:50 -0300

    includ auth router

--- src/routes.js
+++ src/routes.js
@@ -6,10 +6,10 @@ const authUser = require("./middleware/auth");
 //Rotas de usuários
 routes.get("/users/:id", authUser, userController.showUserOne);
 routes.get("/users", authUser, userController.showUser);
-routes.post("/users", userController.store);
+routes.post("/users", authUser, userController.store);
 routes.patch("/users/:id", authUser, userController.updateUser);
 routes.patch("/endUser/:user_id", authUser, userController.updateEndereco);
 routes.post("/logOn", userController.logOn);
-routes.delete("/users/:id", userController.deletUser);
+routes.delete("/users/:id", authUser, userController.deletUser);
 
 module.exports = routes;
