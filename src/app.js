b00bf5c0a5db1d43a40b9cd3cecd1234992235b5
WilkRhu <wilk.caetano@gmail.com>
2020-05-28 18:11:59 -0300

    introducao do novo cros

--- src/app.js
+++ src/app.js
@@ -12,7 +12,15 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
-app.use("/", cors(),  routes);
-app.use(cors());
+app.use("/", routes);
+// app.use(cors());
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 
 module.exports = app;
